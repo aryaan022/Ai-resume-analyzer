8ed70c28592e24557c19496f93a2676910e20ee2
aryaan022 <aryankhokhar022@gmail.com>
2026-07-07 15:55:32 +0530

    LOGIC

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app =express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials:true, //because we are handling cookies and we want to allow cross-origin requests with credentials
 }))
 
--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({ //instance of axios with baseURL and withCredentials
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
     withCredentials: true,
 })
 export async function register({username ,email ,password}) {
